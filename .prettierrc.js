707ac7ca53c72691ccb8e1d2766ce32a9a2492d2
Tanvir Alam <tanvir.alam.shawn@gmail.com>
2023-06-20 21:35:54 +0200

    feat: husky Installing

---
+++ .prettierrc.js
@@ -0,0 +1,13 @@
+const config = {
+    semi: false,
+    tabWidth: 2,
+    printWidth: 120,
+    singleQuote: true,
+    jsxSingleQuote: true,
+    trailingComma: 'none',
+    arrowParens: 'always',
+    endOfLine: 'auto'
+  }
+  
+  module.exports = config
+  
