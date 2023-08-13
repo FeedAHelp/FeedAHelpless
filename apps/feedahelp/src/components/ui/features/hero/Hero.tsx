4de14068bdc0dd8336f6bce20c12a79bf7aaa579
biplob2358 <biplob2358@gmail.com>
2023-08-13 21:29:22 +0600

    feat: make layout

---
+++ apps/feedahelp/src/components/ui/features/hero/Hero.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <div className="h-72 mt-28 w-full border border-red-600">
+      This is the Hero section.
+    </div>
+  );
+};
+
+export default Hero;
