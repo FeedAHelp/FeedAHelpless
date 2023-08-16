a4bb252964367726397a9f4b5e25f1bd5fc4e6ca
Biplob Ghosh <96138959+biplob2358@users.noreply.github.com>
2023-08-16 23:49:02 +0600

    feat: added sponsors component (#175)

---
+++ apps/feedahelp/src/components/ui/features/sponsors/Sponsors.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Sponsors = () => {
+    return (
+        <div className='border border-red-600 w-full h-60 mb-2'>
+            This is the Sponsors section
+        </div>
+    );
+};
+
+export default Sponsors;
