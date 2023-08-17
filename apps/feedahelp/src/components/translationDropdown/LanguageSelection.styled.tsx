c1cd8e159124b6050a593e0c159e474495b3fe1c
Rakibul Hasan <rakibulhasan20162@gmail.com>
2023-08-17 21:59:02 +0600

    fix: changed language icon

--- apps/feedahelp/src/components/translationDropdown/LanguageSelection.styled.tsx
+++ apps/feedahelp/src/components/translationDropdown/LanguageSelection.styled.tsx
@@ -3,18 +3,18 @@ import styled from 'styled-components';
 const HoverImage = styled.div`
   &:hover {
     img {
-      -webkit-filter: invert(10%) sepia(100%) saturate(4000%) hue-rotate(-35deg) grayscale(.350);
-      filter: invert(10%) sepia(100%) saturate(4000%) hue-rotate(-35deg) grayscale(.350);
+      -webkit-filter: invert(30%) sepia(96%) saturate(4000%) hue-rotate(20deg);
+      filter: invert(30%) sepia(96%) saturate(4000%) hue-rotate(20deg);
     }
   }
   &:active {
     img {
-        -webkit-filter: invert(10%) sepia(100%) saturate(4000%) hue-rotate(-30deg) grayscale(.550);
-        filter: invert(10%) sepia(100%) saturate(4000%) hue-rotate(-30deg) grayscale(.550);
+      -webkit-filter: invert(10%) sepia(96%) saturate(4000%) hue-rotate(20deg);
+      filter: invert(10%) sepia(96%) saturate(4000%) hue-rotate(20deg);
     }
   }
 `;
 
 export const Styled = {
-    HoverImage
+    HoverImage,
   };
