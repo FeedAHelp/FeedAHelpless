44285bb86b40719a7b21b83382550acf502f0971
Muksana <muksanaakter3@gmail.com>
2023-08-19 01:42:31 +0600

    updated

--- apps/feedahelp/src/components/ui/features/hero/Hero.tsx
+++ apps/feedahelp/src/components/ui/features/hero/Hero.tsx
@@ -1,11 +1,10 @@
-import backgroundImageUrl from "../../../../../public/assets/hero-cover.png"
+import backgroundImageUrl from "../../../../../public/assets/hero-cover.png";
 
 const Hero = () => {
-
   return (
     <div className="flex ">
       <div className=" bg-neutral-50">
-        <h1 className="font-semibold mt-16 w-32  md:w-96 ps-3 py-10 md:ps-14 lg:text-5xl md:text-3xl text-base text-neutral-400">
+        <h1 className="mt-16 w-32 py-10  ps-3 text-base font-semibold text-neutral-400 md:w-96 md:ps-14 md:text-3xl lg:text-5xl">
           <span className="mb-5 inline-block">Your Recipe,</span> <br />
           <span className="mb-5 inline-block">Our Expertise</span>
           <br />
