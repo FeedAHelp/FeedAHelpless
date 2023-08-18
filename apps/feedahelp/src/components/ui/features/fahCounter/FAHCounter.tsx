981808997cf54fb5ebc84f683d8de4dd9d2a9ed3
Tanvir Alam <tanvir.alam.shawn@gmail.com>
2023-08-18 18:21:47 +0200

    fix: add few test cases and soleve the issue

--- apps/feedahelp/src/components/ui/features/fahCounter/FAHCounter.tsx
+++ apps/feedahelp/src/components/ui/features/fahCounter/FAHCounter.tsx
@@ -7,6 +7,7 @@ const FAHCounter = () => {
     <div className="grid grid-cols-1 gap-8 p-10 lg:grid-cols-2 xl:grid-cols-4">
       {FAHCounterData.map((item, i) => (
         <FAHCounterDisplay
+          key={i}
           title={item?.title}
           imageIcon={item?.imageIcon}
           imageAlt={item?.imageAlt}
--- packages/ui/components/elements/Modal/GenericModal.test.tsx
+++ packages/ui/components/elements/Modal/GenericModal.test.tsx
@@ -1,64 +1,64 @@
-import React from "react";
-import { render } from "react-dom";
-import Modal from "./GenericModal"; // Update the path to your Modal component
+import React from 'react'
+import { render } from '@testing-library/react'
+import Modal from './GenericModal' // Update the path to your Modal component
 
-describe("Modal Component", () => {
-  it("should render the modal content when isOpen is true", () => {
-    const container = document.createElement("div");
+describe('Modal Component', () => {
+  it('should render the modal content when isOpen is true', () => {
+    const container = document.createElement('div')
     render(
       <Modal isOpen={true} closeModal={() => {}}>
         <div>Modal Content</div>
       </Modal>,
       container
-    );
+    )
 
-    const modalContent = container.querySelector(".modal-content");
-    expect(modalContent).not.toBeNull();
-  });
+    const modalContent = container.querySelector('.modal-content')
+    expect(modalContent).not.toBeNull()
+  })
 
-  it("should not render the modal content when isOpen is false", () => {
-    const container = document.createElement("div");
+  it('should not render the modal content when isOpen is false', () => {
+    const container = document.createElement('div')
     render(
       <Modal isOpen={false} closeModal={() => {}}>
         <div>Modal Content</div>
       </Modal>,
       container
-    );
+    )
 
-    const modalContent = container.querySelector(".modal-content");
-    expect(modalContent).toBeNull();
-  });
+    const modalContent = container.querySelector('.modal-content')
+    expect(modalContent).toBeNull()
+  })
 
-  it("should call closeModal function when ModalContainer is clicked", () => {
-    const closeModalMock = jest.fn();
-    const container = document.createElement("div");
+  it('should call closeModal function when ModalContainer is clicked', () => {
+    const closeModalMock = jest.fn()
+    const container = document.createElement('div')
     render(
       <Modal isOpen={true} closeModal={closeModalMock}>
         <div>Modal Content</div>
       </Modal>,
       container
-    );
+    )
 
-    const modalContainer = container.querySelector(".modal-container");
-    modalContainer?.dispatchEvent(new MouseEvent("click"));
+    const modalContainer = container.querySelector('.modal-container')
+    modalContainer?.dispatchEvent(new MouseEvent('click'))
 
-    expect(closeModalMock).toHaveBeenCalled();
-  });
+    expect(closeModalMock).toHaveBeenCalled()
+  })
 
-  it("should stop event propagation when ModalContent is clicked", () => {
-    const closeModalMock = jest.fn();
-    const container = document.createElement("div");
+  it('should stop event propagation when ModalContent is clicked', () => {
+    const closeModalMock = jest.fn()
+    const container = document.createElement('div')
     render(
       <Modal isOpen={true} closeModal={closeModalMock}>
-        <div className="modal-content">Modal Content</div>
+        <div className='modal-content'>Modal Content</div>
       </Modal>,
       container
-    );
+    )
 
-    const modalContent = container.querySelector(".modal-content");
-    const eventMock = { stopPropagation: jest.fn() };
-    modalContent?.dispatchEvent(new MouseEvent("click", eventMock));
+    const modalContent = container.querySelector('.modal-content')
+    const eventMock = { stopPropagation: jest.fn() }
+    modalContent?.dispatchEvent(new MouseEvent('click', eventMock))
 
-    expect(eventMock.stopPropagation).toHaveBeenCalled();
-  });
-});
+    expect(eventMock.stopPropagation).toHaveBeenCalled()
+  })
+})
