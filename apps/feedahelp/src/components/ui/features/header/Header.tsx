6643e123f95df20079f411c6caf986babbf6e98a
Rakibul Hasan <rakibulhasan20162@gmail.com>
2023-08-20 07:01:52 +0600

    fix: navigation blurness when scrolling

--- apps/feedahelp/src/components/ui/features/header/Header.tsx
+++ apps/feedahelp/src/components/ui/features/header/Header.tsx
@@ -1,6 +1,6 @@
 import dynamic from "next/dynamic";
 import { Styled } from "./Header.styled";
-import { memo } from "react";
+import { memo, useEffect, useState } from "react";
 import Navbar from "./navbar/Navbar";
 import Head from "next/head";
 
@@ -8,8 +8,20 @@ const Logo = dynamic(import("./logo/Logo"));
 const MemoLogo = memo(Logo);
 
 export const Header = () => {
+  const [isScrolled, setIsScrolled] = useState<boolean>(false);
+
+  const handleScroll = () => {
+    const scrollPosition = window.scrollY;
+    if (scrollPosition > 60) return setIsScrolled(true);
+    setIsScrolled(false);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+  }, []);
+
   return (
-    <Styled.Header>
+    <Styled.Header scrollBlur={isScrolled}>
       <Head>
         <title>FeedAhelp :: Feed one at a time. v1.0</title>
         <meta name="description" content="" />
