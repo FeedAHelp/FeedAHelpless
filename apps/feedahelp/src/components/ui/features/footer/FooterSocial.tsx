d28afa9e3b0913de88feb558ba80a1cba081bab1
biplob2358 <biplob2358@gmail.com>
2023-08-22 22:15:00 +0600

    refactor: data map

--- apps/feedahelp/src/components/ui/features/footer/FooterSocial.tsx
+++ apps/feedahelp/src/components/ui/features/footer/FooterSocial.tsx
@@ -1,52 +1,43 @@
 import React, { useEffect, useState } from "react";
 import Image from "next/image";
 import { fetchSocialMedia } from "~/utils/cms/fetchSocialMedia";
+import { urlForThumbnail } from "~/utils/cms/imageProcess";
 
 const FooterSocial = () => {
-  const [socialMedia,setSocilaMedia]=useState([])
-  console.log("🚀 ~ file: FooterSocial.tsx:7 ~ FooterSocial ~ socialMedia:", socialMedia)
+  const [socialMedia, setSocilaMedia] = useState([]);
 
-  const getSocilaMedia =async () =>{
+  const getSocilaMedia = async () => {
     try {
-        const socialData = await fetchSocialMedia();
-        setSocilaMedia(socialData);
+      const socialData = await fetchSocialMedia();
+      setSocilaMedia(socialData);
     } catch (error) {
-        console.error(error)
+      console.error(error);
     }
-}
+  };
 
-useEffect(() => {
-  getSocilaMedia()
-  }, [])
+  useEffect(() => {
+    getSocilaMedia();
+  }, []);
   return (
     <div className="flex justify-center space-x-4 pt-4 lg:col-end-13 lg:pt-0">
-      <a rel="noopener noreferrer" href="#" title="Facebook">
-        <Image
-          src={"/static/images/feedahelp/facebook.svg"}
-          width={40}
-          height={40}
-          alt={"feedahelpLogo"}
-          className="h-full object-cover"
-        />
-      </a>
-      <a rel="noopener noreferrer" href="#" title="Email">
-        <Image
-          src={"/static/images/feedahelp/mail.svg"}
-          width={40}
-          height={40}
-          alt={"feedahelpLogo"}
-          className="h-full object-cover"
-        />
-      </a>
-      <a rel="noopener noreferrer" href="#" title="Google">
-        <Image
-          src={"/static/images/feedahelp/twitter.svg"}
-          width={40}
-          height={40}
-          alt={"feedahelpLogo"}
-          className="h-full object-cover"
-        />
-      </a>
+      {socialMedia.map((item: any, key) => {
+        return (
+          <a
+            key={key}
+            rel="noopener noreferrer"
+            href={item.solicalLinkUrl}
+            title={item.title}
+          >
+            <Image
+              src={urlForThumbnail(item.imageIcon)}
+              width={40}
+              height={40}
+              alt={"feedahelpLogo"}
+              className="h-full object-cover"
+            />
+          </a>
+        );
+      })}
     </div>
   );
 };
