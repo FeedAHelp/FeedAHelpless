20f23bcc8930f8f303479e3040cb8a65a48685ca
Rakibul Hasan <108168071+Rakibs511@users.noreply.github.com>
2023-08-24 14:26:06 +0600

    feat: Possible to enter email and subscribe (#214)

---
+++ apps/server/controllers/subscribe/getSubscriber.controller.ts
@@ -0,0 +1,16 @@
+import { Request, Response } from 'express'
+import { prisma } from '../../utills/prismaInstance'
+
+export const getSubscriberController = async (req: Request, res: Response) => {
+  try {
+    const subscriber = await prisma.feedahelpNewsletter.findMany({
+      orderBy: {
+        createAt: 'desc'
+      }
+    })
+    return res.status(200).json(subscriber)
+  } catch (error) {
+    console.log(error)
+    return res.status(500).json({ message: 'Failed to fetch user data' })
+  }
+}
