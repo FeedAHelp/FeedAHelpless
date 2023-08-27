b7809d9fdf58d1fc3ef04ffe0de0aa509e723ffa
Tanvir Alam <tanvir.alam.shawn@gmail.com>
2023-08-27 13:35:57 +0200

    feat: adding social icons

---
+++ apps/feedahelp/src/components/ui/features/userAccess/login/Login.styled.tsx
@@ -0,0 +1,7 @@
+import styled from "@emotion/styled";
+
+const SocialAvatar = styled.div``;
+
+export const Styled = {
+  SocialAvatar,
+};
--- apps/feedahelp/src/components/ui/features/userAccess/login/Login.tsx
+++ apps/feedahelp/src/components/ui/features/userAccess/login/Login.tsx
@@ -1,6 +1,10 @@
 import dynamic from "next/dynamic";
 import { memo, useEffect, useState } from "react";
 import ImageCarousal from "../imageCarousal/imageCarousal";
+import GenericLink from "../../../../../../../../packages/ui/components/elements/GenericLink/GenericLink";
+import ReCAPTCHA from "react-google-recaptcha";
+import Image from "next/image";
+import { Styled } from "./Login.styled";
 
 const Logo = dynamic(import("../../header/logo/Logo"));
 const MemoLogo = memo(Logo);
@@ -17,27 +21,25 @@ const Login = () => {
             <ImageCarousal />
             <p className="mt-10 flex flex-col items-center justify-center text-center">
               <span>Don't have an account?</span>
-              <a href="#" className="underline">
-                Get Started!
-              </a>
+              <GenericLink href={"www.google.com"} color="Black">
+                {"Get Started!"}
+              </GenericLink>
             </p>
-            <p className="mt-6 text-center text-sm text-black">
+            <p className="mt-6 text-center text-black">
               Read our{" "}
-              <a href="#" className="underline">
-                terms
-              </a>{" "}
-              and{" "}
-              <a href="#" className="underline">
-                conditions
-              </a>
+              <GenericLink href={"www.google.com"} color="Black">
+                {"terms"}
+              </GenericLink>
+              <a href="#" className="underline"></a> and{" "}
+              <GenericLink href={"www.google.com"} color="Black">
+                {"conditions"}
+              </GenericLink>
             </p>
           </div>
           <div className="bg-white p-5 md:flex-1">
-            <h3 className="my-4 text-2xl font-semibold text-gray-700">
-              Account Login
-            </h3>
+            <h3 className="my-4 text-2xl font-semibold text-gray-700">Login</h3>
             <form action="#" className="flex flex-col space-y-5">
-              <div className="flex flex-col space-y-1">
+              <div className="flex flex-col text-left">
                 <label
                   htmlFor="email"
                   className="text-sm font-semibold text-gray-500"
@@ -59,12 +61,13 @@ const Login = () => {
                   >
                     Password
                   </label>
-                  <a
-                    href="#"
-                    className="text-sm text-blue-600 hover:underline focus:text-blue-800"
+                  <GenericLink
+                    href={"www.google.com"}
+                    color="Black"
+                    fontSize="0.8rem"
                   >
-                    Forgot Password?
-                  </a>
+                    {"Forgot Password?"}
+                  </GenericLink>
                 </div>
                 <input
                   type="password"
@@ -86,6 +89,9 @@ const Login = () => {
                 </label>
               </div>
               <div>
+                <ReCAPTCHA sitekey="Your client site key" />
+              </div>
+              <div>
                 <button
                   type="submit"
                   className="w-full rounded-md bg-blue-500 px-4 py-2 text-lg font-semibold text-white shadow transition-colors duration-300 hover:bg-blue-600 focus:outline-none focus:ring-4 focus:ring-blue-200"
@@ -102,45 +108,56 @@ const Login = () => {
                   <span className="h-px w-14 bg-gray-400"></span>
                 </span>
                 <div className="flex flex-col space-y-4">
-                  <a
-                    href="#"
-                    className="group flex items-center justify-center space-x-2 rounded-md border border-gray-800 px-4 py-2 transition-colors duration-300 hover:bg-gray-800 focus:outline-none"
-                  >
-                    <span>
-                      <svg
-                        className="h-5 w-5 fill-current text-gray-800 group-hover:text-white"
-                        viewBox="0 0 16 16"
-                        version="1.1"
-                        aria-hidden="true"
-                      >
-                        <path
-                          fill-rule="evenodd"
-                          d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z"
-                        ></path>
-                      </svg>
-                    </span>
-                    <span className="text-sm font-medium text-gray-800 group-hover:text-white">
-                      Github
-                    </span>
-                  </a>
-                  <a
-                    href="#"
-                    className="group flex items-center justify-center space-x-2 rounded-md border border-blue-500 px-4 py-2 transition-colors duration-300 hover:bg-blue-500 focus:outline-none"
-                  >
-                    <span>
-                      <svg
-                        className="text-blue-500 group-hover:text-white"
-                        width="20"
-                        height="20"
-                        fill="currentColor"
-                      >
-                        <path d="M6.29 18.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0020 3.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.073 4.073 0 01.8 7.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 010 16.407a11.616 11.616 0 006.29 1.84"></path>
-                      </svg>
-                    </span>
-                    <span className="text-sm font-medium text-blue-500 group-hover:text-white">
-                      Twitter
-                    </span>
-                  </a>
+                  <div className="grid grid-cols-3 gap-2">
+                    <div className="flex items-center justify-center">
+                      <Image
+                        src="/static/images/social/facebook.png"
+                        alt="Facebook"
+                        width={48}
+                        height={48}
+                      />
+                    </div>
+                    <div className="flex items-center justify-center">
+                      <Image
+                        src="/static/images/social/google.png"
+                        alt="Facebook"
+                        width={48}
+                        height={48}
+                      />
+                    </div>
+                    <div className="flex items-center justify-center">
+                      <Image
+                        src="/static/images/social/Instogram.png"
+                        alt="Facebook"
+                        width={48}
+                        height={48}
+                      />
+                    </div>
+                    <div className="flex items-center justify-center">
+                      <Image
+                        src="/static/images/social/linkedIn.png"
+                        alt="Facebook"
+                        width={48}
+                        height={48}
+                      />
+                    </div>
+                    <div className="flex items-center justify-center">
+                      <Image
+                        src="/static/images/social/Twitter.png"
+                        alt="Facebook"
+                        width={48}
+                        height={48}
+                      />
+                    </div>
+                    <div className="flex items-center justify-center">
+                      <Image
+                        src="/static/images/social/Youtube.png"
+                        alt="Facebook"
+                        width={48}
+                        height={48}
+                      />
+                    </div>
+                  </div>
                 </div>
               </div>
             </form>
