fa86409ec94e7a0fdee1dc905aa3fa3cc96735e7
Rakibul Hasan <rakibulhasan20162@gmail.com>
2023-08-28 22:26:43 +0600

    refactor: change px to rem

--- apps/feedahelp/src/components/ui/features/fahCounter/FAHCounterDisplay.styled.tsx
+++ apps/feedahelp/src/components/ui/features/fahCounter/FAHCounterDisplay.styled.tsx
@@ -2,21 +2,21 @@ import styled from "styled-components";
 
 const CardWrapper = styled.div`
   display: block;
-  top: 0px;
+  top: 0rem;
   position: relative;
   background-color: #f2f8f9;
-  border-radius: 4px;
+  border-radius: 0.25rem;
   text-decoration: none;
   min-width:2.2rem;
   z-index: 0;
   overflow: hidden;
-  border: 1px solid #f2f8f9;
+  border: 0.0625rem solid #f2f8f9;
 
   &:hover {
     transition: all 0.2s ease-out;
-    box-shadow: 0px 4px 8px rgba(38, 38, 38, 0.2);
-    top: -4px;
-    border: 1px solid rgba(86, 215, 116, 0.598);
+    box-shadow: 0rem 0.25rem 0.5rem rgba(38, 38, 38, 0.2);
+    top: -0.25rem;
+    border: 0.0625rem solid rgba(86, 215, 116, 0.598);
     background-color: white;
   }
 
@@ -24,11 +24,11 @@ const CardWrapper = styled.div`
     content: "";
     position: absolute;
     z-index: -1;
-    bottom: 32px;
-    right: -16px;
-    height: 30px;
-    width: 0px;
-    border-radius: 10px;
+    bottom: 2rem;
+    right: -1rem;
+    height: 1.875rem;
+    width: 0rem;
+    border-radius: 0.625rem;
     transform: scale(2);
     transition: 0.5s ease-out;
   }
@@ -36,12 +36,12 @@ const CardWrapper = styled.div`
     content: "";
     position: absolute;
     z-index: -1;
-    bottom: 32px;
-    right: -16px;
+    bottom: 2rem;
+    right: -1rem;
     background: rgba(86, 215, 116, 0.292);
-    height: 30px;
-    width: 60px;
-    border-radius: 10px;
+    height: 1.875rem;
+    width: 3.75rem;
+    border-radius: 0.625rem;
   }
 
   &:hover:before {
