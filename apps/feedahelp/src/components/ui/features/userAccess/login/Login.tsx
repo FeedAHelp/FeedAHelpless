75ed7241b01ffa92f2787fc2736fa6eadd61a97b
Tanvir Alam <tanvir.alam.shawn@gmail.com>
2023-08-28 16:45:03 +0200

    feat: refacgor data and makeing registion

--- apps/feedahelp/src/components/ui/features/userAccess/login/Login.tsx
+++ apps/feedahelp/src/components/ui/features/userAccess/login/Login.tsx
@@ -13,32 +13,28 @@ const MemoLogo = memo(Logo);
 const Login = () => {
   return (
     <div className="max flex flex-col overflow-hidden rounded-md bg-white shadow-lg md:flex-1 md:flex-row lg:max-w-screen-md">
-      <div className="bg-[#F3AF9A] text-black md:flex md:w-80 md:flex-shrink-0 md:flex-col md:items-center md:justify-center gap-8">
+      <div className="gap-8 bg-[#F3AF9A] text-black md:flex md:w-80 md:flex-shrink-0 md:flex-col md:items-center md:justify-center">
         <MemoLogo />
         <ImageCarousal />
       </div>
       <div className="bg-white p-5 md:flex-1">
-          <RegisterForm />
-          <div className="mb-4 flex flex-row items-center justify-center text-center gap-4 pt-4">
-            <h4 className="text-sm">Don't have an account?</h4>
-            <GenericLink
-              href={"www.google.com"}
-              color="Black"
-              fontSize="0.8rem"
-            >
-              {"Get Started!"}
-            </GenericLink>
+        <RegisterForm />
+        <div className="mb-4 flex flex-row items-center justify-center gap-4 pt-4 text-center">
+          <h4 className="text-sm">Already have an account?</h4>
+          <GenericLink href={"www.google.com"} color="Black" fontSize="0.8rem">
+            {"Sign In!"}
+          </GenericLink>
+        </div>
+        <div className="flex flex-col space-y-5 pt-4">
+          <span className="flex items-center justify-center space-x-2">
+            <span className="h-px w-14 bg-gray-400"></span>
+            <span className="font-normal text-gray-500">or login with</span>
+            <span className="h-px w-14 bg-gray-400"></span>
+          </span>
+          <div className="flex flex-col space-y-4">
+            <SocialMedia />
           </div>
-          <div className="flex flex-col space-y-5 pt-4">
-            <span className="flex items-center justify-center space-x-2">
-              <span className="h-px w-14 bg-gray-400"></span>
-              <span className="font-normal text-gray-500">or login with</span>
-              <span className="h-px w-14 bg-gray-400"></span>
-            </span>
-            <div className="flex flex-col space-y-4">
-              <SocialMedia />
-            </div>
-          </div>
+        </div>
       </div>
     </div>
   );
--- packages/ui/components/elements/PasswordInput/PasswordInput.tsx
+++ packages/ui/components/elements/PasswordInput/PasswordInput.tsx
@@ -1,83 +1,55 @@
-import React, { useState }from "react";
-import { Styled } from "./PasswordInput.styled";
-import {Icon} from 'react-icons-kit';
-import {eyeOff} from 'react-icons-kit/feather/eyeOff';
-import {eye} from 'react-icons-kit/feather/eye'
+import React, { useState } from 'react'
+import { Styled } from './PasswordInput.styled'
+import { Icon } from 'react-icons-kit'
+import { eyeOff } from 'react-icons-kit/feather/eyeOff'
+import { eye } from 'react-icons-kit/feather/eye'
+import PasswordStrengthBar from 'react-password-strength-bar'
 
 type PasswordInputProps = {
-  placeholder: string;
-  strengthCheck?: boolean;
-};
+  placeholder: string
+  strengthCheck?: boolean
+}
 
-export function PasswordInput({placeholder, strengthCheck}: PasswordInputProps) {
-  const [password, setPassword] = useState("");
-    const [type, setType] = useState('password');
-    const [icon, setIcon] = useState(eyeOff);
-    const [message, setMessage] = useState("");
-    const [progress, setProgress] = useState("");
+export function PasswordInput({ placeholder, strengthCheck }: PasswordInputProps) {
+  const [password, setPassword] = useState('')
+  const [type, setType] = useState('password')
+  const [icon, setIcon] = useState(eyeOff)
+  const [message, setMessage] = useState('')
+  const [progress, setProgress] = useState('')
 
-    const handleToggle = () => {
-        if (type==='password'){
-           setIcon(eye);
-           setType('text')
-        } else {
-           setIcon(eyeOff)
-           setType('password')
-        }
+  const handleToggle = () => {
+    if (type === 'password') {
+      setIcon(eye)
+      setType('text')
+    } else {
+      setIcon(eyeOff)
+      setType('password')
     }
+  }
 
-    const handlePassword = (passwordValue) => {
-      const strengthChecks = {
-        length: 0,
-        hasUpperCase: false,
-        hasLowerCase: false,
-        hasDigit: false,
-        hasSpecialChar: false,
-      };
-  
-      strengthChecks.length = passwordValue.length >= 8 ? true : false;
-      strengthChecks.hasUpperCase = /[A-Z]+/.test(passwordValue);
-      strengthChecks.hasLowerCase = /[a-z]+/.test(passwordValue);
-      strengthChecks.hasDigit = /[0-9]+/.test(passwordValue);
-      strengthChecks.hasSpecialChar = /[^A-Za-z0-9]+/.test(passwordValue);
-  
-      let verifiedList = Object.values(strengthChecks).filter((value) => value);
-  
-      let strength =
-        verifiedList.length == 5
-          ? "Strong"
-          : verifiedList.length >= 2
-          ? "Medium"
-          : "Weak";
-  
-      setPassword(passwordValue);
-      setProgress(`${(verifiedList.length / 5) * 100}%`);
-      setMessage(strength);
-  
-      console.log("verifiedList: ", `${(verifiedList.length / 5) * 100}%`);
-    };
+  const handlePassword = (passwordValue) => {
+    setPassword(passwordValue)
+  }
 
-    const getActiveColor = (type) => {
-      if (type === "Strong") return "#8BC926";
-      if (type === "Medium") return "#FEBD01";
-      return "#FF0054";
-    };
-    
   return (
     <>
       <Styled.Field>
-          <Styled.LoginInput
-              type={type}
-              placeholder={placeholder}
-              value={password}
-              onChange={(e) => !strengthCheck ? setPassword(e.target.value) : handlePassword(e.target.value)}
-              autoComplete="current-password"
-          />
-              <span className="flex justify-end items-center cursor-pointer" onClick={handleToggle}>
-                  <Icon className="absolute pb-8" icon={icon} size={30}/>
-              </span>
-          <Styled.Line />
+        <Styled.LoginInput
+          type={type}
+          placeholder={placeholder}
+          value={password}
+          onChange={(e) => (!strengthCheck ? setPassword(e.target.value) : handlePassword(e.target.value))}
+          autoComplete='current-password'
+        />
+        <span onClick={handleToggle}>
+          <Icon icon={icon} size={30} />
+        </span>
+        <PasswordStrengthBar
+          password={password}
+          shortScoreWord='No Password'
+          scoreWords={['ok', 'fine', 'cool', 'too much', 'fuck off']}
+        />
       </Styled.Field>
     </>
-  );
+  )
 }
