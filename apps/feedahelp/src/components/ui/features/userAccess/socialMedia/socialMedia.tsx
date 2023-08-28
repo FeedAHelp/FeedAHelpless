0299693e5848f583cc5073e2e82ade3aeae4afb8
mrmontasir <montasir@softzino.com>
2023-08-29 01:51:15 +0600

    fix(src/components/ui/features/useraccess): fix login with social media icon issue

--- apps/feedahelp/src/components/ui/features/userAccess/socialMedia/socialMedia.tsx
+++ apps/feedahelp/src/components/ui/features/userAccess/socialMedia/socialMedia.tsx
@@ -1,15 +1,45 @@
-import React from "react";
-import { items } from "./data";
-import Image from "next/image";
+import React, { useEffect, useState } from "react";
+import { fetchSocialMedia } from "~/utils/cms/fetchSocialMedia";
+import Link from "next/link";
+import {Styled} from './SocialMedia.styled';
+import { urlForThumbnail } from "~/utils/cms/imageProcess";
 
 const SocialMedia = () => {
+  const [socialMedia, setSocialMedia] = useState([]);
+
+  const getSocialMedia = async () => {
+    try {
+      const socialData = await fetchSocialMedia();
+      setSocialMedia(socialData);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    getSocialMedia();
+  }, []);
+  
   return (
-    <div className="grid grid-cols-3 gap-2">
-      {items.map((value, index) => (
-        <div className="flex items-center justify-center" key={index}>
-          <Image src={value.imgSrc} alt={value.alt} width={48} height={48} />
-        </div>
-      ))}
+    <div className="grid grid-cols-3 gap-2 ml-8">
+     {socialMedia.map((item: any, key) => {
+        return (
+          <Link
+            key={key}
+            rel="noopener noreferrer"
+            href={item.solicalLinkUrl}
+            title={item.title}
+          >
+            <Styled.SocialIcon
+              className="flex items-center justify-center"
+              src={urlForThumbnail(item.imageIcon)}
+              width={48}
+              height={48}
+              alt={"feedahelpLogo"}
+            />
+          </Link>
+        );
+      })}
     </div>
   );
 };
