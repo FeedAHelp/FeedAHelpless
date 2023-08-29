c9a51046e4d7ec0c1d5b72176bad0c559282d1ca
Tanvir Alam <tanvir.alam.shawn@gmail.com>
2023-08-29 23:16:25 +0200

    fix: adding disabled status

--- apps/feedahelp/src/components/ui/features/userAccess/socialMedia/data.ts
+++ apps/feedahelp/src/components/ui/features/userAccess/socialMedia/data.ts
@@ -3,31 +3,37 @@ export const items = [
       imgSrc: "/static/images/social/facebook.png",
       alt: "facebook",
       provider: "facebook",
+      disabled: false
     },
     {
       imgSrc: "/static/images/social/google.png",
       alt: "google",
       provider: "google",
+      disabled: false
     },
     {
       imgSrc: "/static/images/social/Twitter.png",
       alt: "twitter",
       provider: "twitter",
+      disabled: true
     },
     {
       imgSrc: "/static/images/social/linkedIn.png",
       alt: "linkedin",
       provider: "linkedin",
+      disabled: false
     },
     {
       imgSrc: "/static/images/social/Instogram.png",
       alt: "instagram",
       provider: "instagram",
+      disabled: true
     },
     {
       imgSrc: "/static/images/social/YouTube.png",
       alt: "youtube",
       provider: "youtube",
+      disabled: true
     }
   ];
   
