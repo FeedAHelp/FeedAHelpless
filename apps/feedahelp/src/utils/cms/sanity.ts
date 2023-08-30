84ebc07144761fbd68a8dd0d5430aab1dd7f1031
tanviralam <tanvir.alam.shawn@gmail.com>
2023-08-30 13:09:51 +0200

    fix: addign apiVersion for sanity

--- apps/feedahelp/src/utils/cms/sanity.ts
+++ apps/feedahelp/src/utils/cms/sanity.ts
@@ -5,6 +5,7 @@ export const config = {
     projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
     token: process.env.NEXT_PUBLIC_SANITY_API_TOKEN!,
     useCdn: true,
+    apiVersion: '2023-08-30'
 }
 export const sanityClient = createClient(config);
 export const urlFor = (source: string) => createImageUrlBuilder(config).image(source);
