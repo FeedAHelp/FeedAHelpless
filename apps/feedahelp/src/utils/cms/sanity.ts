e34c29989e30c9572977525200951ea79ee345ad
tanviralam <tanvir.alam.shawn@gmail.com>
2023-08-30 12:43:17 +0200

    fix: adding douments and github credential

--- apps/feedahelp/src/utils/cms/sanity.ts
+++ apps/feedahelp/src/utils/cms/sanity.ts
@@ -1,10 +1,10 @@
 import { createClient } from 'next-sanity'
 import createImageUrlBuilder from "@sanity/image-url"
 export const config = {
-    dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
-    projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID! || "s98tqz9i",
+    dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "feedahelp",
+    projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
     token: process.env.NEXT_PUBLIC_SANITY_API_TOKEN!,
-    useCdn: process.env.NODE_ENV === "production",
+    useCdn: process.env.NODE_ENV === "development",
 }
 export const sanityClient = createClient(config);
 export const urlFor = (source: string) => createImageUrlBuilder(config).image(source);
