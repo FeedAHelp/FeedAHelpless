98342c42c9497c6a625618497d9f38cf4425de68
Tanvir Alam <tanvir.alam.shawn@gmail.com>
2023-08-31 14:50:59 +0200

    feat: adding back the pold credetials

--- apps/feedahelp/src/utils/cms/sanity.ts
+++ apps/feedahelp/src/utils/cms/sanity.ts
@@ -1,5 +1,6 @@
-import { createClient } from 'next-sanity'
-import createImageUrlBuilder from "@sanity/image-url"
+import { createClient } from "next-sanity";
+import createImageUrlBuilder from "@sanity/image-url";
+
 export const config = {
     dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "feedahelp",
     projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
@@ -8,5 +9,6 @@ export const config = {
     apiVersion: '2023-08-30',
     ignoreBrowserTokenWarning: true
 }
+
 export const sanityClient = createClient(config);
 export const urlFor = (source: string) => createImageUrlBuilder(config).image(source);
