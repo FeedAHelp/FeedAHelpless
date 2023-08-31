921fe8645db98f9fb2065fd0193b7f575fbaf556
biplob2358 <biplob2358@gmail.com>
2023-08-31 23:00:41 +0600

    refactor: sloved fetch data problem

--- apps/feedahelp/src/utils/cms/sanity.ts
+++ apps/feedahelp/src/utils/cms/sanity.ts
@@ -4,7 +4,7 @@ import createImageUrlBuilder from "@sanity/image-url";
 export const config = {
     dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "feedahelp",
     projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
-    token: process.env.NEXT_PUBLIC_SANITY_API_TOKEN!,
+    // token: process.env.NEXT_PUBLIC_SANITY_API_TOKEN!,
     useCdn: true,
     apiVersion: '2023-08-30',
     ignoreBrowserTokenWarning: true
