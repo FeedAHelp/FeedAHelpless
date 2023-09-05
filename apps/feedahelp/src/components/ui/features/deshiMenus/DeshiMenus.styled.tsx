8ecf6752a9f18c43ec58ecf82dcb0819ab5c7198
tanviralam <tanvir.alam.shawn@gmail.com>
2023-09-05 09:15:15 +0200

    fix: px to rem

--- apps/feedahelp/src/components/ui/features/deshiMenus/DeshiMenus.styled.tsx
+++ apps/feedahelp/src/components/ui/features/deshiMenus/DeshiMenus.styled.tsx
@@ -1,59 +1,63 @@
 import styled, { keyframes } from "styled-components";
 import Image from "next/image";
 
-const iconwidth = "20px";
-const iconheight = "20px";
-const height = "24px";
-const fontsize = "12px";
+const iconwidth = "1.25rem";
+const iconheight = "1.25rem";
+const height = "1.5rem";
+const fontsize = "0.75rem";
 const fontweight = "500";
 const iterationCount = "infinite";
 
+const BoxShadow = () => `
+  box-shadow: -0.0625rem -0.0625rem 0.125rem rgba(255, 255, 255, 0.25),
+      inset 0.125rem 0.125rem 0.3125rem rgba(255, 255, 255, 0.25),
+      0.5rem 1.875rem 1.875rem rgba(0, 0, 0, 0.4), inset -0.125rem -0.125rem 0.3125rem rgba(0, 0, 0, 0.4);
+`;
+
 const Container = styled.div`
-  width: 280px;
-  height: 500px;
-  margin: 40px auto;
-  border-radius: 10px;
+  width: 17.5rem;
+  height: 31.25rem;
+  margin: 2.5rem auto;
+  border-radius: 0.625rem;
 `;
 
 const Logo = styled.img`
-  width: 55px;
-  margin-top: 25px;
+  width: 3.4375rem;
+  margin-top: 1.5625rem;
   transition: transform 0.9s;
 `;
 
 const Title = styled.div`
-  font-size: 10px;
+  font-size: 0.625rem;
   color: #fff;
   transition: transform 0.5s;
   transform: scale(1);
   font-weight: 500;
-  font-size: 19px;
-  padding-top: 20px;
+  font-size: 1.1875rem;
+  padding-top: 1.25rem;
 `;
 
 const Body = styled.div`
   position: relative;
   z-index: 0;
   background: #fff;
-  height: 240px;
-  border-radius: 10px;
-  padding-top: 30px;
-  padding-left: 10px;
-  padding-right: 10px;
-  box-shadow: -1px -1px 2px rgba(255, 255, 255, 0.25),
-    inset 2px 2px 5px rgba(255, 255, 255, 0.25),
-    8px 30px 30px rgba(0, 0, 0, 0.4), inset -2px -2px 5px rgba(0, 0, 0, 0.4);
+  height: 15rem;
+  border-radius: 0.625rem;
+  padding-top: 1.875rem;
+  padding-left: 0.625rem;
+  padding-right: 0.625rem;
+  ${BoxShadow}
 `;
 
 const Badge = styled.span`
   position: relative;
   background: red;
   color: #fff;
-  font-size: 8px;
-  padding: 3px 10px;
-  top: -3px;
-  border-radius: 3px;
-  margin-left: 7px;
+  font-size: 0.5rem;
+  padding: 0.1875rem 0.625rem;
+  top: -0.1875rem;
+  border-radius: 0.1875rem;
+  margin-left: 0.4375rem;
 `;
 
 const Caption = styled.div`
@@ -61,26 +65,24 @@ const Caption = styled.div`
   display: grid;
   grid-template-columns: repeat(4, 1fr);
   grid-template-rows: 1fr;
-  grid-row-gap: 5px;
-  padding: 5px;
+  grid-row-gap: 0.3125rem;
+  padding: 0.3125rem;
 `;
 
 const Rating = styled.div`
-  padding-top: 7px;
-  font-size: 12px;
+  padding-top: 0.4375rem;
+  font-size: 0.75rem;
 `;
 
 const Price = styled.div`
   width: 100%;
-  font-size: 15px;
+  font-size: 0.9375rem;
   background: #fa792e;
   text-align: center;
   font-weight: 300;
   color: #fff;
-  border-radius: 7px;
-  box-shadow: -1px -1px 2px rgba(255, 255, 255, 0.25),
-    inset 2px 2px 5px rgba(255, 255, 255, 0.25),
-    8px 30px 30px rgba(0, 0, 0, 0.4), inset -2px -2px 5px rgba(0, 0, 0, 0.4);
+  border-radius: 0.4375rem;
+  ${BoxShadow}
 `;
 
 const DeshiShareIcon = styled(Image)`
@@ -99,26 +101,24 @@ const DeshiUl = styled.ul`
   z-index: 1000;
   display: flex;
   justify-content: center;
-  gap: 10px;
-  bottom: -15px;
-  padding-left: 10px;
+  gap: 0.625rem;
+  bottom: -0.9375rem;
+  padding-left: 0.625rem;
 `;
 
 const DeshiLi = styled.li`
   background: #fff;
   border-radius: 50%;
-  padding: 8px;
-  box-shadow: -1px -1px 2px rgba(255, 255, 255, 0.25),
-    inset 2px 2px 5px rgba(255, 255, 255, 0.25),
-    8px 30px 30px rgba(0, 0, 0, 0.4), inset -2px -2px 5px rgba(0, 0, 0, 0.4);
+  padding: 0.5rem;
+  ${BoxShadow}
 `;
 
 const pulseAuxiliarLow = keyframes`
   0% {
-    box-shadow: 0px 0px 0px 0px rgba(179, 62, 150, 1);
+    box-shadow: 0 0 0 0 rgba(179, 62, 150, 1);
   }
   100% {
-    box-shadow: 0px 0px 0px 5px transparent;
+    box-shadow: 0 0 0 0.3125rem transparent;
   }
 `;
 
@@ -147,7 +147,7 @@ const Highlight = styled.div`
   }
 
   &::after {
-    border: 1px solid #fff;
+    border: 0.0625rem solid #fff;
     animation: ${pulseAuxiliarLow} 1s ease ${iterationCount};
   }
 `;
@@ -157,14 +157,14 @@ const StatusTag = styled.div`
   width: auto;
   display: inline-flex;
   height: ${height};
-  padding: 0 10px 0 5px;
+  padding: 0 0.625rem 0 0.3125rem;
   align-items: center;
   justify-content: center;
-  gap: 5px;
-  border-top-left-radius: 13px;
-  border-bottom-left-radius: 13px;
-  border-top-right-radius: 13px;
-  border-bottom-right-radius: 13px;
+  gap: 0.3125rem;
+  border-top-left-radius: 0.8125rem;
+  border-bottom-left-radius: 0.8125rem;
+  border-top-right-radius: 0.8125rem;
+  border-bottom-right-radius: 0.8125rem;
 `;
 
 const StatusTagText = styled.span`
@@ -185,7 +185,7 @@ const Heading = styled.div`
   gap: 1rem;
 
   &:hover > ${Logo} {
-    transform: scale(6) translate(25px, 10px);
+    transform: scale(6) translate(1.5625rem, 0.625rem);
   }
 
   &:hover > ${Title} {
@@ -202,15 +202,13 @@ const Heading = styled.div`
 const Header = styled.div`
   position: relative;
   z-index: 1;
-  height: 180px;
-  border-radius: 10px;
+  height: 11.25rem;
+  border-radius: 0.625rem;
   background-color: #fa792e;
   background-image: linear-gradient(left top, #fa792e 17%, #bf212f 100%);
   cursor: pointer;
-  padding-top: 1px;
-  box-shadow: -1px -1px 2px rgba(255, 255, 255, 0.25),
-    inset 2px 20px 5px rgba(255, 255, 255, 0.25),
-    8px 30px 30px rgba(0, 0, 0, 0.4), inset -2px -2px 5px rgba(0, 0, 0, 0.4);
+  padding-top: 0.0625rem;
+  ${BoxShadow}
 `;
 
 export const Styled = {
