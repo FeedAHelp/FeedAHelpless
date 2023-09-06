13c305e26100bfb56cd3152d1fbe7fdfb3cf9285
Nd Morsalin <121650325+ND-Morsalin@users.noreply.github.com>
2023-09-06 21:34:49 +0600

    230 bugs update the sponsor section (#261)
    * style:make it mobile responsive
    
    * scale on hover images
    
    * fixed: empty the test file
    
    * fixed: empty the test file
    
    * fix: delete test file
    
    * fix: unwanted code is remove
    
    * fix: rotating animation is added
    
    * fix: best practice is followed according to given instructin

--- apps/feedahelp/src/components/ui/features/sponsors/Sponsors.tsx
+++ apps/feedahelp/src/components/ui/features/sponsors/Sponsors.tsx
@@ -1,22 +1,26 @@
-import React from "react";
-import { Styled } from "./Sponsers.styled";
 import { items } from "./data";
 import Image from "next/image";
+import CircularText from "./circularText/circularText";
 
 const Sponsors = () => {
   return (
     <div className="mb-2 border">
       <div className="container mx-auto p-4">
-        <div className="grid grid-cols-3 gap-4">
+        <div className="grid grid-cols-2 md:grid-cols-3 gap-4 ">
           {items.map((value, index) => (
-            <Styled.bgCover key={index}>
+            <div
+              className="sponsor-wrapper"
+              key={index}
+            >
               <Image
                 src={value.imgSrc}
                 alt={value.alt}
-                width={200}
-                height={200}
+                width={150}
+                height={150}
+                className={`rounded-full md:w-[150px] md:h-[150px] w-[110px] h-[110px]`}
               />
-            </Styled.bgCover>
+              <CircularText text={value.text} />
+            </div>
           ))}
         </div>
       </div>
