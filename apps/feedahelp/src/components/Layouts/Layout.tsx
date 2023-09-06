4eeb53eef77af59f11ec4f63ef0cfb5b9f8b6f52
Nazmus Sakib <126310362+MNSM92@users.noreply.github.com>
2023-09-06 11:10:34 -0400

    fix: Change the generic spinner (#281)
    * fix: Change the generic spinner
    
    * fix: reviewed
    
    * fix: review done

--- apps/feedahelp/src/components/Layouts/Layout.tsx
+++ apps/feedahelp/src/components/Layouts/Layout.tsx
@@ -1,6 +1,9 @@
 import type { ReactNode } from "react";
+import { useContext } from "react";
 import type { ThemeName } from "~/ui/components/foundations/theming";
 import { ThemeProvider } from "~/ui/components/contexts/ThemeContext";
+import { useLoadingContext } from "~/ui/components/contexts/LoadingContext";
+import CustomSpinner from "~/ui/components/elements/GenericSpinner/CustomSpinner";
 
 type LayoutProps = {
   theme?: ThemeName;
@@ -16,11 +19,14 @@ type LayoutProps = {
 };
 
 export function Layout({ header, child, footer }: LayoutProps) {
+  const { isLoading } = useLoadingContext();
+
   return (
     <ThemeProvider>
       <header>{header}</header>
       <main>{child}</main>
       <footer>{footer}</footer>
+      <CustomSpinner isLoading={isLoading}></CustomSpinner>
     </ThemeProvider>
   );
 }
