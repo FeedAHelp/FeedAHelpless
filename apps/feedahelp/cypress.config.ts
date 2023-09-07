512266f5b9d8694c18bf5b64c043f57b0bff7db0
tanviralam <tanvir.alam.shawn@gmail.com>
2023-09-07 09:55:23 +0200

    fix: adding end to end cfigration

--- apps/feedahelp/cypress.config.ts
+++ apps/feedahelp/cypress.config.ts
@@ -1,11 +1,18 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
-  projectId: 'dhjcdj',
+  projectId: "dhjcdj",
+
   component: {
     devServer: {
       framework: "next",
       bundler: "webpack",
+    },
+  },
+
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
     },
   },
 });
