6ebe7d94986c0f9d7cc1b9b9902cf3e01329e0fd
Tanvir Alam <tanvir.alam.shawn@gmail.com>
2023-09-08 23:02:57 +0200

    fix: adding dist to gitignore

--- apps/server/dist/routes/user.router.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const getAllusers_controller_1 = require("../controllers/user/getAllusers.controller");
-const userRouter = (0, express_1.Router)();
-userRouter.get('/getAllUsers', getAllusers_controller_1.getAllUsersController);
-exports.default = userRouter;
-//# sourceMappingURL=user.router.js.map
--- apps/server/dist/schema/subscribe.schema.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.subscribeSchema = void 0;
-const zod_1 = require("zod");
-exports.subscribeSchema = zod_1.z.object({
-    email: zod_1.z.string().email().min(5)
-});
-//# sourceMappingURL=subscribe.schema.js.map
