fc991800e46fe8dcfe91c1cee55515cc388be97f
Rakibul Hasan <rakibulhasan20162@gmail.com>
2023-09-12 12:20:14 +0600

    fix: login with google

--- apps/feedahelp/src/pages/api/auth/[...nextauth].ts
+++ apps/feedahelp/src/pages/api/auth/[...nextauth].ts
@@ -13,124 +13,116 @@ import {
 import { type NextAuthOptions } from "next-auth";
 import { postMethod } from "../../../utils/api/postMethod";
 import { endPoints } from "../../../utils/api/route";
-import CredentialsProvider from 'next-auth/providers/credentials'
+import CredentialsProvider from "next-auth/providers/credentials";
 
 export const authOptions: NextAuthOptions = {
-    session: {
-      strategy: "jwt",
-      maxAge: 30 * 24 * 60 * 60, // 30 days
-    },
-    providers: [
-      GoogleProvider({
-        clientId: `${process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID}`,
-        clientSecret: `${process.env.NEXT_PUBLIC_GOOGLE_CLIENT_SECRET}`,
-      }),
-      FacebookProvider({
-        clientId: `${process.env.NEXT_PUBLIC_FACEBOOK_ID}`,
-        clientSecret: `${process.env.NEXT_PUBLIC_FACEBOOK_SECRET}`,
-      }),
-      InstagramProvider({
-        clientId: `${process.env.NEXT_PUBLIC_INSTAGRAM_ID}`,
-        clientSecret: `${process.env.NEXT_PUBLIC_INSTAGRAM_SECRET}`,
-      }),
-      TwitterProvider({
-        clientId: `${process.env.NEXT_PUBLIC_TWITTER_ID}`,
-        clientSecret: `${process.env.NEXT_PUBLIC_TWITTER_SECRET}`,
-      }),
-      LinkedInProvider({
-        clientId: `${process.env.NEXT_PUBLIC_LINKEDIN_ID}`,
-        clientSecret: `${process.env.NEXT_PUBLIC_LINKEDIN_SECRET}`,
-      }),
-      Discord({
-        clientId: `${process.env.DISCORD_CLIENT_ID}`,
-        clientSecret: `${process.env.DISCORD_CLIENT_SECRET}`,
-      }),
-      CredentialsProvider({
-        type: 'credentials',
-        credentials: {},
-        async authorize(credentials: any) {
-          const user = {
-            ...credentials,
-          }
-          return user
-        },
-      }),
-    ],
-    callbacks: {
-      jwt: async ({ token, user }: JWTCallbackParams) => {
-        if (user && (user.id || user.name)) {
-          await postMethod(endPoints.auth.register, {
-            name: user.name || "Not found",
-            email: user.email,
-            password: user.password || user.id,
-            image: user.image || "Not found",
-            role: "User",
-          }).then((res) => {
-            console.log(res);
-            
-            /* const data: User = res.data;
-            token.id = data.id;
-            token.email = data.email;
-            token.name = data.name;
-            token.userType = data.registerId;
-            token.image = data.image;
-            token.accessToken = data.accessToken;  */
-          })
-         /*  await postMethod(endPoints.auth.register, {
-            name: user.name || "Not found",
-            email: user.email,
-            password: user.password || user.id,
-            image: user.image || "Not found",
-            role: "Customer",
-          })
-            .then((res) => {
-              console.log(res.data)
-              const data: User = res.data;
-              token.id = data.id;
-              token.email = data.email;
-              token.name = data.name;
-              token.userType = data.registerId;
-              token.image = data.image;
-              token.accessToken = data.accessToken; 
-            })
-            .catch(async (error) => {
-              console.error(error);
-            }); */
-        } 
-/*         if (user && (!user.id || !user.name)) {
-          await postMethod(endPoints.auth.login, {
-            email: user.email,
-            password: user.password || user.id,
-          })
-            .then((res) => {
-              const data: User = res.data;
-              token.id = data.id;
-              token.email = data.email;
-              token.name = data.name;
-              token.userType = data.registerId;
-              token.image = data.image;
-              token.accessToken = data.accessToken;
-            })
-            .catch((error) => {
-              console.error(error.response.data.message);
-              token.error = error.response.data.message;
-            });
-        } */
-        return token;
+  session: {
+    strategy: "jwt",
+    maxAge: 30 * 24 * 60 * 60, // 30 days
+  },
+  providers: [
+    GoogleProvider({
+      clientId: `${process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID}`,
+      clientSecret: `${process.env.NEXT_PUBLIC_GOOGLE_CLIENT_SECRET}`,
+    }),
+    FacebookProvider({
+      clientId: `${process.env.NEXT_PUBLIC_FACEBOOK_ID}`,
+      clientSecret: `${process.env.NEXT_PUBLIC_FACEBOOK_SECRET}`,
+    }),
+    InstagramProvider({
+      clientId: `${process.env.NEXT_PUBLIC_INSTAGRAM_ID}`,
+      clientSecret: `${process.env.NEXT_PUBLIC_INSTAGRAM_SECRET}`,
+    }),
+    TwitterProvider({
+      clientId: `${process.env.NEXT_PUBLIC_TWITTER_ID}`,
+      clientSecret: `${process.env.NEXT_PUBLIC_TWITTER_SECRET}`,
+    }),
+    LinkedInProvider({
+      clientId: `${process.env.NEXT_PUBLIC_LINKEDIN_ID}`,
+      clientSecret: `${process.env.NEXT_PUBLIC_LINKEDIN_SECRET}`,
+    }),
+    Discord({
+      clientId: `${process.env.DISCORD_CLIENT_ID}`,
+      clientSecret: `${process.env.DISCORD_CLIENT_SECRET}`,
+    }),
+    CredentialsProvider({
+      name: "credentials",
+      type: "credentials",
+      credentials: {},
+      async authorize(credentials: any) {
+        const user = {
+          ...credentials,
+        };
+        return user;
       },
-      session: ({ session, token }: SessionCallbackParams) => {
-        if (token) {
-          session.user.id = token.id;
-          session.user.email = token.email;
-          session.user.name = token.name;
-          session.user.userType = token.userType;
-          session.user.image = token.image;
-          session.user.accessToken = token.accessToken;
-          session.user.error = token.error;
-        }
-        return session;
-      },
+    }),
+  ],
+  callbacks: {
+    async jwt({ token, user, account }) {
+      //   if (user && (user.id || user.name)) {
+      //     await postMethod(endPoints.auth.register, {
+      //       name: user.name || "Not found",
+      //       email: user.email,
+      //       password:  user.id,
+      //       image: user.image || "Not found",
+      //       role: "User",
+      //     }).then((res) => {
+      //         console.log(res);
+      //       const data: User = res.data;
+      //       token.id = data.id;
+      //       token.email = data.email;
+      //       token.name = data.name;
+      //       token.userType = data.registerId;
+      //       token.image = data.image;
+      //       token.accessToken = data.accessToken;
+      //     });
+      //    /*  await postMethod(endPoints.auth.register, {
+      //       name: user.name || "Not found",
+      //       email: user.email,
+      //       password: user.password || user.id,
+      //       image: user.image || "Not found",
+      //       role: "Customer",
+      //     })
+      //       .then((res) => {
+      //         console.log(res.data)
+      //         const data: User = res.data;
+      //         token.id = data.id;
+      //         token.email = data.email;
+      //         token.name = data.name;
+      //         token.userType = data.registerId;
+      //         token.image = data.image;
+      //         token.accessToken = data.accessToken;
+      //       })
+      //       .catch(async (error) => {
+      //         console.error(error);
+      //       }); */
+      //   }
+
+      // when upper comment lines are work then you can remove this is block
+      if (user) {
+        token.id = user.id;
+        token.email = user.email;
+        token.name = user.name;
+        token.userType = account?.type;
+        token.picture = user.image;
+        token.accessToken = account?.access_token;
+      }
+      return token;
     },
-  };
-  
+
+    async session({ session, token }: SessionCallbackParams) {
+      if (token) {
+        session.user.id = token.id;
+        session.user.email = token.email;
+        session.user.name = token.name;
+        session.user.userType = token.userType;
+        session.user.image = token.picture as string;
+        session.user.accessToken = token.accessToken;
+        session.user.error = token.error;
+      }
+      return session;
+    },
+  },
+};
+
 export default NextAuth(authOptions);
