f6237e617dc2328a05b3bf69b06b93adfa97a52f
Tanvir Alam <tanvir.alam.shawn@gmail.com>
2023-09-23 16:31:45 +0200

    feat: adding twitter and discrord icon

--- apps/feedahelp/src/components/ui/features/userAccess/socialMedia/socialMedia.tsx
+++ apps/feedahelp/src/components/ui/features/userAccess/socialMedia/socialMedia.tsx
@@ -1,21 +1,26 @@
 import React from "react";
 import { signIn } from "next-auth/react";
-import { items } from "./data";
+import { SocialMedias } from "./socialMediaData";
 import { Styled } from "./socialMedia.styled";
 
 const SocialMedia = () => {
-  const handleSignIn = (e: React.MouseEvent<HTMLDivElement, MouseEvent>, provider: string) => {
+  const handleSignIn = (
+    e: React.MouseEvent<HTMLDivElement, MouseEvent>,
+    provider: string
+  ) => {
     e.preventDefault();
     signIn(provider, { callbackUrl: "/" });
   };
 
   return (
     <div className="grid grid-cols-3 gap-2">
-      {items.map((item, index) => (
+      {SocialMedias.map((item, index) => (
         <div
           className="flex cursor-pointer items-center justify-center"
           key={index}
-          onClick={(e) => !item.disabled ? handleSignIn(e, item.provider) : null}
+          onClick={(e) =>
+            !item.disabled ? handleSignIn(e, item.provider) : null
+          }
         >
           <Styled.SocialIcon
             src={item.imgSrc}
