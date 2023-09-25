a62b9dc67cda467f042731a3a04a25a7f193132e
tanviralam <tanvir.alam.shawn@gmail.com>
2023-09-25 12:15:49 +0200

    feat: adding payment componennet

---
+++ apps/feedahelp/src/components/ui/features/payments/Payment.styled.tsx
@@ -0,0 +1,10 @@
+import styled from "@emotion/styled";
+
+const PaymentWrapper = styled.div`
+  position: relative;
+  padding-top: 1rem;
+`;
+
+export const Styled = {
+    PaymentWrapper,
+};
---
+++ apps/feedahelp/src/components/ui/features/payments/Payment.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { Styled } from "./Payment.styled";
+
+const Payment = () => {
+  return <Styled.PaymentWrapper>Index</Styled.PaymentWrapper>;
+};
+
+export default Payment;
