43e8aa1c7f744a814f91036ea8c5ddb99c9db8cb
Md. Mohiuddin <mohiuddin.ice.ru@gmail.com>
2023-09-26 09:39:21 +0600

    feat: ingredient model added

---
+++ apps/server/models/IngredientModel.ts
@@ -0,0 +1,59 @@
+import { PrismaClient } from '@prisma/client'
+import Ingredient from './Ingredient'
+const prisma = new PrismaClient()
+
+class IngredientModel {
+  private ingredients: Ingredient[] = []
+
+  async exists(id: number): Promise<boolean> {
+    const numberOfIngredients: number = await prisma.ingredient.count({
+      where: { id }
+    })
+    if (numberOfIngredients > 0) return true
+    return false
+  }
+
+  async create(data: { name: string; imageName: string }): Promise<Ingredient> {
+    const createdIngredient = await prisma.ingredient.create({
+      data: {
+        name: data.name,
+        imageName: data.imageName
+      }
+    })
+    return new Ingredient(createdIngredient.id, createdIngredient.name, createdIngredient.imageName)
+  }
+
+  async read(id: number): Promise<Ingredient | null> {
+    const ingredient = await prisma.ingredient.findUnique({
+      where: { id }
+    })
+    if (!ingredient) return null
+    return new Ingredient(ingredient.id, ingredient.name, ingredient.imageName)
+  }
+
+  async update(id: number, data: { name: string; imageName: string }): Promise<Ingredient | null> {
+    const updatedIngredient = await prisma.ingredient.update({
+      where: { id },
+      data: {
+        name: data.name,
+        imageName: data.imageName
+      }
+    })
+    if (!updatedIngredient) return null
+    return new Ingredient(updatedIngredient.id, updatedIngredient.name, updatedIngredient.imageName)
+  }
+
+  async delete(id: number): Promise<boolean> {
+    const deleteResult = await prisma.ingredient.delete({
+      where: { id }
+    })
+    return !!deleteResult
+  }
+
+  async getAll(): Promise<Ingredient[]> {
+    const ingredients = await prisma.ingredient.findMany()
+    return ingredients.map((ingredient) => new Ingredient(ingredient.id, ingredient.name, ingredient.imageName))
+  }
+}
+
+export default IngredientModel
