243197be5ed51dbe3b1742c42b0de308fadb750a
Md. Mohiuddin <mohiuddin.ice.ru@gmail.com>
2023-09-26 09:38:02 +0600

    feat: Ingredient Type added

---
+++ apps/server/models/Ingredient.ts
@@ -0,0 +1,13 @@
+class Ingredient {
+  id: number
+  name: string
+  imageName: string
+
+  constructor(id: number, name: string, imageName: string) {
+    this.id = id
+    this.name = name
+    this.imageName = imageName
+  }
+}
+
+export default Ingredient
