227d9eeab884e1e42310206d15218e47d5a47b8f
Md. Mohiuddin <mohiuddin.ice.ru@gmail.com>
2023-09-29 20:19:36 +0600

    feat: added all ingredient  route

--- apps/server/routes/ingredient.router.ts
+++ apps/server/routes/ingredient.router.ts
@@ -1,10 +1,12 @@
 import express, { Router } from 'express'
 import { IngredientController } from '../controllers/ingredient/ingredient.controller'
+import IngredientModel from 'models/IngredientModel'
 
 export const ingredientRouter: Router = express.Router()
 
 const ingredientController = new IngredientController()
 
-ingredientRouter.get('/:id', ingredientController.getAIngredient)
+ingredientRouter.get('/all', ingredientController.getAllIngredients)
+ingredientRouter.get('/:id', ingredientController.getIngredient)
 
 export default ingredientRouter
