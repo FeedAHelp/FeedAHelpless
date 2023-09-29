941635fdb6268eb47b65b66b04f3a731ae0893f6
Md. Mohiuddin <mohiuddin.ice.ru@gmail.com>
2023-09-29 10:32:41 +0600

    fix: removed old migration files
    old migration files was cluttering and creating errors. As we are in dev mode, we don't need to keep
    track of the db structure so much.

--- apps/server/prisma/migrations/20230925183044_paypal/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "Payment" (
-    "id" SERIAL NOT NULL,
-    "orderID" TEXT NOT NULL,
-    "status" TEXT NOT NULL,
-
-    CONSTRAINT "Payment_pkey" PRIMARY KEY ("id")
-);
