a338600d9b24f87895c262b8f5bc43f4986b65ef
Md. Mohiuddin <mohiuddin.ice.ru@gmail.com>
2023-09-29 11:35:07 +0600

    refactor: seeding in bulk
    creating many connections for each seeding element is costly

--- apps/server/prisma/seeder.ts
+++ apps/server/prisma/seeder.ts
@@ -1,44 +1,31 @@
 import { PrismaClient } from '@prisma/client'
 import fsPromises from 'fs/promises'
 import path from 'path'
-import ProgressBar from 'progress'
 
 const prisma = new PrismaClient()
 
-export async function seed({ file, schema }: { file: string; schema: string }) {
-  const filePath = path.join(process.cwd(), file)
-  const jsonData = await fsPromises.readFile(filePath)
-  const rawData = JSON.parse(jsonData.toString())
-  const data = rawData[schema]
-  const totalData = data.length
-  const prismaTable = (prisma as any)[schema]
-
-  await prismaTable.deleteMany()
-
-  const bar = new ProgressBar('  inserting [:bar] :rate/bps :percent :etas', {
-    complete: '=',
-    incomplete: ' ',
-    width: 20,
-    total: totalData
+async function insertAll(schemaName: string, entries: Object[]): Promise<any> {
+  const model = (prisma as any)[schemaName]
+  if (!model) return false
+  const createdEntries: Object[] = await model.createMany({
+    data: entries
   })
+  return createdEntries.length > 0
+}
 
-  const createEntry = async (i: any) => {
-    try {
-      await prismaTable.create({
-        data: i
-      })
-      bar.tick()
-    } catch (error) {
-      console.error(`Error inserting ${schema}: ${error.message}`)
+export async function seed({ file, schema }: { file: string; schema: string }) {
+  try {
+    const filePath = path.join(process.cwd(), file)
+    const fileData = await fsPromises.readFile(filePath)
+
+    const jsonData = JSON.parse(fileData.toString())
+    const data = jsonData[schema]
+
+    if (await insertAll(schema, data)) {
+      console.log(`All ${schema} inserted successfully.`)
     }
+    await prisma.$disconnect()
+  } catch (e) {
+    console.log('Error seeding', e)
   }
-
-  const insertAll = async () => {
-    const promises = data.map((i: any) => createEntry(i))
-    await Promise.all(promises)
-    console.log(`All ${schema} inserted successfully.`)
-  }
-  await insertAll()
-
-  await prisma.$disconnect()
 }
