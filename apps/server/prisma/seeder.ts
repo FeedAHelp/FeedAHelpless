ce0349c236b5d304cc178ebd4266fba997224fe7
Md. Mohiuddin <mohiuddin.ice.ru@gmail.com>
2023-09-29 12:17:19 +0600

    refactor: seeder updated
    some points are updated
    1. skipDuplicates will help use to throw error in seeding elements that
       exists
    2. removed deleteMany because it will throw us error
        because of foreign key constraint
    3. disconnected the database only after all our work has been finished
        because connecting- disconnecting is a costly operation
    4. moved all console.logs after the main method
        otherwise it is cluttering our code

--- apps/server/prisma/seeder.ts
+++ apps/server/prisma/seeder.ts
@@ -7,25 +7,23 @@ const prisma = new PrismaClient()
 async function insertAll(schemaName: string, entries: Object[]): Promise<any> {
   const model = (prisma as any)[schemaName]
   if (!model) return false
-  const createdEntries: Object[] = await model.createMany({
-    data: entries
+
+  const createdEntries = await model.createMany({
+    data: entries,
+    skipDuplicates: true
   })
   return createdEntries.length > 0
 }
 
 export async function seed({ file, schema }: { file: string; schema: string }) {
-  try {
-    const filePath = path.join(process.cwd(), file)
-    const fileData = await fsPromises.readFile(filePath)
+  const filePath = path.join(process.cwd(), file)
+  const fileData = await fsPromises.readFile(filePath)
 
-    const jsonData = JSON.parse(fileData.toString())
-    const data = jsonData[schema]
+  const jsonData = JSON.parse(fileData.toString())
+  const data = jsonData[schema]
+  const model = (prisma as any)[schema]
 
-    if (await insertAll(schema, data)) {
-      console.log(`All ${schema} inserted successfully.`)
-    }
-    await prisma.$disconnect()
-  } catch (e) {
-    console.log('Error seeding', e)
+  if (await insertAll(schema, data)) {
+    console.log(`All ${schema} inserted successfully.`)
   }
 }
