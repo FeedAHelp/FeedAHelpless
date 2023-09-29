a2e269586a093adfaf672e4417d536f65a986669
Md. Mohiuddin <mohiuddin.ice.ru@gmail.com>
2023-09-29 20:18:37 +0600

    feat: all ingredients route

--- apps/server/controllers/ingredient/ingredient.controller.ts
+++ apps/server/controllers/ingredient/ingredient.controller.ts
@@ -2,9 +2,14 @@ import IngredientModel from '../../models/IngredientModel'
 import type { Request, Response } from 'express'
 
 export class IngredientController extends IngredientModel {
-  async getAIngredient(req: Request, res: Response) {
+  async getIngredient(req: Request, res: Response) {
     const { id } = req.params
     const ingredient = await super.read(id)
     res.status(200).json(ingredient)
+  }
+
+  async getAllIngredients(req: Request, res: Response) {
+    const ingredients = await super.allIngredients()
+    res.status(200).json(ingredients)
   }
 }
