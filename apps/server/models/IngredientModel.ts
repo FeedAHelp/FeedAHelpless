6b3ebf4e5e67cfadd364ebb7a82d11a88ecbefbe
Md. Mohiuddin <mohiuddin.ice.ru@gmail.com>
2023-10-01 00:31:48 +0600

    refactor: Ingredient model rewritten making Ingredient an interface

--- apps/server/models/IngredientModel.ts
+++ apps/server/models/IngredientModel.ts
@@ -1,60 +1,102 @@
 import { PrismaClient } from '@prisma/client'
-import Ingredient from './Ingredient'
 const prisma = new PrismaClient()
 
+interface Ingredient {
+  id: string
+  name: string
+  imageName: string
+}
+
 class IngredientModel {
-  async allIngredients() {
-    return await prisma.ingredient.findMany({})
+  async allIngredients(): Promise<Ingredient[]> {
+    return await prisma.ingredient.findMany()
   }
 
   async exists(id: string): Promise<boolean> {
     const numberOfIngredients: number = await prisma.ingredient.count({
       where: { id }
     })
-    if (numberOfIngredients > 0) return true
-    return false
+    return numberOfIngredients > 0
   }
 
-  async create(data: { id: string; name: string; imageName: string }): Promise<Ingredient> {
-    const createdIngredient = await prisma.ingredient.create({
-      data: {
-        name: data.name,
-        imageName: data.imageName
+  async create(data: { name: string; imageName: string }): Promise<Ingredient | null> {
+    try {
+      const createdIngredient = await prisma.ingredient.create({
+        data: {
+          name: data.name,
+          imageName: data.imageName
+        }
+      })
+
+      return {
+        id: createdIngredient.id,
+        name: createdIngredient.name,
+        imageName: createdIngredient.imageName
       }
-    })
-    return new Ingredient(createdIngredient.id, createdIngredient.name, createdIngredient.imageName)
+    } catch (error) {
+      console.error(error)
+      return null
+    }
   }
 
   async read(id: string): Promise<Ingredient | null> {
     const ingredient = await prisma.ingredient.findUnique({
       where: { id }
     })
+
     if (!ingredient) return null
-    return new Ingredient(ingredient.id, ingredient.name, ingredient.imageName)
+
+    return {
+      id: ingredient.id,
+      name: ingredient.name,
+      imageName: ingredient.imageName
+    }
   }
 
   async update(id: string, data: { name: string; imageName: string }): Promise<Ingredient | null> {
-    const updatedIngredient = await prisma.ingredient.update({
-      where: { id },
-      data: {
-        name: data.name,
-        imageName: data.imageName
+    try {
+      const updatedIngredient = await prisma.ingredient.update({
+        where: { id },
+        data: {
+          name: data.name,
+          imageName: data.imageName
+        }
+      })
+
+      if (!updatedIngredient) return null
+
+      return {
+        id: updatedIngredient.id,
+        name: updatedIngredient.name,
+        imageName: updatedIngredient.imageName
       }
-    })
-    if (!updatedIngredient) return null
-    return new Ingredient(updatedIngredient.id, updatedIngredient.name, updatedIngredient.imageName)
+    } catch (error) {
+      console.error(error)
+      return null
+    }
   }
 
   async delete(id: string): Promise<boolean> {
-    const deleteResult = await prisma.ingredient.delete({
-      where: { id }
-    })
-    return !!deleteResult
+    try {
+      await prisma.ingredient.delete({
+        where: { id }
+      })
+
+      return true
+    } catch (error) {
+      console.error(error)
+      return false
+    }
   }
 
   async getAll(): Promise<Ingredient[]> {
     const ingredients = await prisma.ingredient.findMany()
-    return ingredients.map((ingredient) => new Ingredient(ingredient.id, ingredient.name, ingredient.imageName))
+
+    return ingredients.map((ingredient) => ({
+      id: ingredient.id,
+      name: ingredient.name,
+      imageName: ingredient.imageName
+    }))
   }
 }
 
