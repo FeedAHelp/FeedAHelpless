9a5b97f47d4bb8bebe478f53e800f688fe2be59e
MNSM92 <mnsm.pbl@gmail.com>
2023-10-01 18:40:09 +0600

    fix: stripe

--- apps/server/controllers/stripe/stripe.controller.ts
+++ apps/server/controllers/stripe/stripe.controller.ts
@@ -11,45 +11,35 @@ export default async function handler(
 ) {
     if (req.method === 'POST') {
         try {
-          const params = {
-            submit_type: 'pay',
-            mode: 'payment',
-            payment_method_types: ['card'],
-            billing_address_collection: 'auto',
-            shipping_options: [
-              {},
-            ],
-            line_items: req.body.map((item) => {
-              const img = item.image[0].asset._ref;
-              const newImage = img.replace('image-', 'https://cdn.sanity.io/images/vfxfwnaw/production/').replace('-webp', '.webp');
+      const params = {
+        submit_type: 'pay',
+        mode: 'payment',
+        payment_method_types: ['card'],
+        billing_address_collection: 'auto',
+        shipping_options: [
+          { },
+        ],
+        line_items: [{
+          price_data: {
+            currency: 'usd',
+            product_data: {
+              name: "item.name",
+            },
+            unit_amount: 100,
+          },
+          quantity: 1
+        }],
+        success_url: `${req.headers.origin}/success`,
+        cancel_url: `${req.headers.origin}/canceled`,
+      }
 
-              return {
-                price_data: {
-                  currency: 'usd',
-                  product_data: {
-                    name: item.name,
-                    images: [newImage],
-                  },
-                  unit_amount: item.price * 100,
-                },
-                adjustable_quantity: {
-                  enabled:true,
-                  minimum: 1,
-                },
-                quantity: item.quantity
-              }
-            }),
-            success_url: `${req.headers.origin}/success`,
-            cancel_url: `${req.headers.origin}/canceled`,
-          }
+      // Create Checkout Sessions from body params.
+      const session = await stripe.checkout.sessions.create(params);
 
-          // Create Checkout Sessions from body params.
-          const session = await stripe.checkout.sessions.create(params);
-
-          res.status(200).json(session);
-        } catch (err) {
-          res.status(err.statusCode || 500).json(err.message);
-        }
+      res.status(200).json(session);
+    } catch (err) {
+      res.status(err.statusCode || 500).json(err.message);
+    }
       } else {
         res.setHeader('Allow', 'POST');
         res.status(405).end('Method Not Allowed');
