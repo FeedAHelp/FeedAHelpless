239645e46eb061a374a64adfadc4f0b68ac60c9c
Md. Mohiuddin <mohiuddin.ice.ru@gmail.com>
2023-10-04 23:02:42 +0600

    fix: added error handling with try catch

--- apps/server/controllers/ingredient/ingredient.controller.ts
+++ apps/server/controllers/ingredient/ingredient.controller.ts
@@ -3,13 +3,23 @@ import type { Request, Response } from 'express'
 
 export class IngredientController extends IngredientModel {
   async getIngredient(req: Request, res: Response) {
-    const { id } = req.params
-    const ingredient = await super.read(id)
-    res.status(200).json(ingredient)
+    try {
+      const { id } = req.params
+      const ingredient = await super.read(id)
+      res.status(200).json(ingredient)
+    } catch (error) {
+      console.log(error)
+      res.status(500).json('Internal Server Error')
+    }
   }
 
   async getAllIngredients(req: Request, res: Response) {
-    const ingredients = await super.allIngredients()
-    res.status(200).json(ingredients)
+    try {
+      const ingredients = await super.allIngredients()
+      res.status(200).json(ingredients)
+    } catch (error) {
+      console.log(error)
+      res.status(500).json('Internal Server Error')
+    }
   }
 }
