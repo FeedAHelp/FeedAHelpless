c00e565de0895daca889a6b0d37d36fdbe2570c3
urmirahman <urmi.rahman690@gmail.com>
2023-10-07 22:33:42 +0600

    fix: redesign sponsor done

--- apps/feedahelp/src/components/ui/features/fahCounter/FAHCounterDisplay.tsx
+++ apps/feedahelp/src/components/ui/features/fahCounter/FAHCounterDisplay.tsx
@@ -21,26 +21,23 @@ const CounterDisplay = ({
 }: CounterDisplayProp) => {
   const { t } = useTranslation("sponsor");
   return (
-    <Styled.CardWrapper>
-      <Styled.CardContain>
-        <div>
-          <Styled.CardHeadingH6>{t(title)}</Styled.CardHeadingH6>
+    <Styled.CardFAH>
+      <div className="card">
+        <div className="img relative">
+          <Image
+            className="object-contain"
+            fill
+            src={urlForThumbnail(imageIcon)}
+            alt=""
+          />
+        </div>
+        <h2>{t(title)}</h2>
+        <div className="cont">
           <Styled.CardCounter end={value} />
-          <Styled.CardMonthlyInc>{monthlyIncrease}</Styled.CardMonthlyInc>
+          <p>{monthlyIncrease}</p>
         </div>
-        <div>
-          <span>
-            <Image
-              src={urlForThumbnail(imageIcon)}
-              alt={"imageAlt"}
-              width={50}
-              height={50}
-              className="h-auto w-auto"
-            />
-          </span>
-        </div>
-      </Styled.CardContain>
-    </Styled.CardWrapper>
+      </div>
+    </Styled.CardFAH>
   );
 };
 
