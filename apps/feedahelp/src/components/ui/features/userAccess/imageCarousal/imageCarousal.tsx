5b89b3549a7c8fa52db43132d7e285b05d8f2faa
Tanvir Alam <TanvirAlam@users.noreply.github.com>
2023-10-07 17:25:17 +0200

    fix: update the login screen (#393)

--- apps/feedahelp/src/components/ui/features/userAccess/imageCarousal/imageCarousal.tsx
+++ apps/feedahelp/src/components/ui/features/userAccess/imageCarousal/imageCarousal.tsx
@@ -1,22 +1,20 @@
 import React from "react";
-import Image from 'next/image'
+import Image from "next/image";
 import { items } from "./data";
 
 const ImageCarousal = () => {
   return (
-    <div>
-      <div className="gallery sm:rounded-e-full">
-        {items.map((value, index) => (
-          <Image
-            className="img"
-            src={value.imgSrc}
-            alt="a lovely kiss in the night"
-            key={index}
-            width={172}
-            height={172}
-          />
-        ))}
-      </div>
+    <div className="gallery sm:rounded-e-full">
+      {items.map((value, index) => (
+        <Image
+          className="img"
+          src={value.imgSrc}
+          alt="a lovely kiss in the night"
+          key={index}
+          width={172}
+          height={172}
+        />
+      ))}
     </div>
   );
 };
