b18664314309464fb78508349a4064fe6d066f2f
Tanvir Alam <tanvir.alam.shawn@gmail.com>
2023-10-07 16:47:06 +0200

    fix: finilize the cms

--- apps/feedahelp/src/components/ui/features/deshiMenus/MainContent/MainContent.tsx
+++ apps/feedahelp/src/components/ui/features/deshiMenus/MainContent/MainContent.tsx
@@ -1,10 +1,22 @@
 import React, { useState, useEffect } from "react";
 import { Styled } from "./MainContent.styled";
 import { fetchDeshiMenusMainContent } from "~/utils/cms/fetchDeshiMenusMainContent";
-import { urlForThumbnail } from "~/utils/cms/imageProcess";
 
-const MainContent = ({ mainContents }: any) => {
-  const [DeshiMenusMainContents, setDeshiMenusMainContent] = useState([]);
+interface MainContentProps {
+  mainContents: Array<any>; // Replace 'any' with the actual type of mainContents
+}
+
+interface DeshiMenuContent {
+  _id: string;
+  imgName: string;
+  contentName: string;
+  contentValue: string;
+}
+
+const MainContent = ({ mainContents }: MainContentProps) => {
+  const [DeshiMenusMainContents, setDeshiMenusMainContent] = useState<
+    Array<DeshiMenuContent>
+  >([]);
 
   const getDeshiMenusMainContent = async () => {
     try {
@@ -22,21 +34,28 @@ const MainContent = ({ mainContents }: any) => {
   return (
     <Styled.MainContent>
       <Styled.ListWithIconsUL>
-        {DeshiMenusMainContents.map((item, index) => {
-          return (
-            <Styled.ListWithIconsLI key={index}>
-              <Styled.DeshiShareIcon
-                src={`/static/images/misc/` + item.imgName}
-                width={30}
-                height={30}
-                alt={item.contentName}
-              />
-              <Styled.TextSpan>{item.contentName}</Styled.TextSpan>
-              <Styled.TextInformation>
-                {item.contentValue}
-              </Styled.TextInformation>
-            </Styled.ListWithIconsLI>
+        {mainContents.map((item, index) => {
+          const deshiMenuContent = DeshiMenusMainContents.find(
+            (elem) => item._ref === elem._id
           );
+          if (deshiMenuContent) {
+            return (
+              <Styled.ListWithIconsLI key={index}>
+                <Styled.DeshiShareIcon
+                  src={`/static/images/misc/${deshiMenuContent.imgName}`}
+                  width={30}
+                  height={30}
+                  alt={deshiMenuContent.contentName}
+                />
+                <Styled.TextSpan>
+                  {deshiMenuContent.contentName}
+                </Styled.TextSpan>
+                <Styled.TextInformation>
+                  {deshiMenuContent.contentValue}
+                </Styled.TextInformation>
+              </Styled.ListWithIconsLI>
+            );
+          }
         })}
       </Styled.ListWithIconsUL>
     </Styled.MainContent>
