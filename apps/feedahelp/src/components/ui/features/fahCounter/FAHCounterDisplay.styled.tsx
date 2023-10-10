ca225869141a4cf30f577811b60f1502c1a228b7
Sojib Mondol <mdsojibmandol@gmail.com>
2023-10-10 14:39:21 +0600

    feat: 397-animate-the-icon-of-the-fahcounter

--- apps/feedahelp/src/components/ui/features/fahCounter/FAHCounterDisplay.styled.tsx
+++ apps/feedahelp/src/components/ui/features/fahCounter/FAHCounterDisplay.styled.tsx
@@ -27,23 +27,32 @@ const CardFAH = styled.div`
     margin: 1rem;
     background-color: ${({ theme }) => theme.colors.neutrals.gray_black};
     color: ${({ theme }) => theme.colors.neutrals.full_white};
+    
     .img {
       width: 5rem;
       height: 5rem;
       border-radius: 50%;
-      background: linear-gradient(
-        to right,
-        ${({ theme }) => theme.colors.tertiary.lightBlue},
-        ${({ theme }) => theme.colors.tertiary.lightBrown}
-      );
       padding: 0.313rem;
       margin-top: -2.813rem;
+      transition: box-shadow 0.4s, background-color 0.4s, border-color 0.2s; 
+      background: white;
+      border: 1px solid currentColor;
     }
+
     h2 {
       margin: 0.625rem 0;
     }
 
     &:hover {
+      & > .img {
+        box-shadow:
+          0 0 2px #fff,
+          0 -2px 6px #f16427,
+          2px 0 6px #36ff00,
+          -2px 0 6px #e22d91,
+          0 2px 6px #3f2aff;
+        border-color: transparent;
+      }
       & > .cont {
         display: flex;
         flex-direction: column;
