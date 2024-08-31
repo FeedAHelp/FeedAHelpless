7a603bec616ea956b35d08a5b0b6690322786343
Tanvir Alam <tanvir.alam.shawn@gmail.com>
2024-08-31 15:40:17 +0200

    fix: remove the string

--- apps/feedahelp-mobile/app/+not-found.tsx
+++ apps/feedahelp-mobile/app/+not-found.tsx
@@ -1,21 +1,21 @@
-import { Link, Stack } from 'expo-router';
-import { StyleSheet } from 'react-native';
+import { Link, Stack } from 'expo-router'
+import { StyleSheet } from 'react-native'
 
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+import { ThemedText } from '@/components/ThemedText'
+import { ThemedView } from '@/components/ThemedView'
 
 export default function NotFoundScreen() {
   return (
     <>
       <Stack.Screen options={{ title: 'Oops!' }} />
       <ThemedView style={styles.container}>
-        <ThemedText type="title">This screen doesn't exist.</ThemedText>
-        <Link href="/" style={styles.link}>
-          <ThemedText type="link">Go to home screen!</ThemedText>
+        <ThemedText type='title'>This screen doesnt exist.</ThemedText>
+        <Link href='/' style={styles.link}>
+          <ThemedText type='link'>Go to home screen!</ThemedText>
         </Link>
       </ThemedView>
     </>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
@@ -23,10 +23,10 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
-    padding: 20,
+    padding: 20
   },
   link: {
     marginTop: 15,
-    paddingVertical: 15,
-  },
-});
+    paddingVertical: 15
+  }
+})
