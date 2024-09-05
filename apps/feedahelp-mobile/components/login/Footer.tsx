dd6cd12838a5e814b713db5594b921c2fa1e221e
Dani025a <dani025a@edu.easj.dk>
2024-09-05 15:20:06 +0200

    Updated commit message or changes

---
+++ apps/feedahelp-mobile/components/login/Footer.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default function Footer() {
+  return (
+    <View>
+      <Text>
+        Footer
+      </Text>
+    </View>
+  );
+}
---
+++ apps/feedahelp-mobile/components/login/Form.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default function Form() {
+  return (
+    <View>
+      <Text>
+        Form
+      </Text>
+    </View>
+  );
+}
