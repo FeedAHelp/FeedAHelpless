09b1138246cca36064853730acc68394934c3906
khal71 <khal0934@edu.zealand.dk>
2024-09-28 22:47:14 +0200

    feat(component): made a new file anad coponent

--- apps/feedahelp-mobile/components/login/Footer.tsx
+++ apps/feedahelp-mobile/components/login/Footer.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+import InstagramLoginButton from '../SocialMedia/InstagramLoginButton';
 
 export default function Footer() {
   return (
     <View>
       <Text>
         Footer
+        <InstagramLoginButton />
       </Text>
     </View>
-  );
-}
+  )};
